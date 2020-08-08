271898ad721882cb86aee2e44deb34e2e83d39b6
littwa <littwa777@gmail.com>
2020-08-08 14:58:24 +0300

    Updates

---
+++ precache-manifest.f26ccbe49a63cdc42a8659c7723c4ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f5635858fb6efa1bf8a1e9c41310b2",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "a416b58162e937ac99d9",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.7a61cb2e.chunk.css"
+  },
+  {
+    "revision": "5186c0f433880897a8a1",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.8bfc141e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.8bfc141e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a416b58162e937ac99d9",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.0321995f.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
